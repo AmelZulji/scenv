a03cb4bd13ca670733e88233a64cce4a56d96f6c
AmelZulji <zuljiamel1991@gmail.com>
2024-09-25 16:51:54 +0200

    expand function to build docker with rstudio

--- build_multiomems_paper.sh
+++ build_multiomems_paper.sh
@@ -1,3 +1,8 @@
 docker build -f dockerfiles/multiomems_paper1.0.Dockerfile -t amelzulji/multiomems_paper:1.0 .
 docker login
 docker push amelzulji/multiomems_paper:1.0
+
+
+docker build -f dockerfiles/multiomems_paper1.0rs.Dockerfile -t amelzulji/multiomems_paper:1.0rs .
+docker login
+docker push amelzulji/multiomems_paper:1.0rs
