fb694ca807cdc227f5d19bab5dbabe85cf8ae4fa
AmelZulji <zuljiamel1991@gmail.com>
2024-10-01 10:19:27 +0200

    add --no-cache flag

--- build_multiomems_paper.sh
+++ build_multiomems_paper.sh
@@ -1,4 +1,4 @@
-docker build -f dockerfiles/multiomems_paper1.0.Dockerfile -t amelzulji/multiomems_paper:1.0 .
+docker build --no-cache -f dockerfiles/multiomems_paper1.0.Dockerfile -t amelzulji/multiomems_paper:1.0 .
 docker login
 docker push amelzulji/multiomems_paper:1.0
 
